3dd4a0e86294e4760c384d9f0d3626f5fcc4495d
Anand Mohan <andmhn@outlook.com>
2024-08-03 19:33:25 +0530

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Footer from './components/footer'
 import Navbar from './components/navbar'
 import Body from './components/body/body'
+import ScrollToTop from "react-scroll-to-top";
 
 function App() {
   return (
     <>
      <Navbar/>
      <Body/>
+     <ScrollToTop smooth />
      <Footer/>
     </>
   )
