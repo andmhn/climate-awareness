1347ef7609fee661d228294d68fbd5c4e8fbd7b6
praweensoni01 <ask.psoni@gmail.com>
2024-08-05 10:17:35 +0530

    bugs fixed

--- src/components/body/heading.jsx
+++ src/components/body/heading.jsx
@@ -5,7 +5,6 @@ import scrollById from '../../utils'
 const heading = () => {
   return (
       <div className="intro" >
-
         <p id="welcome">Climate Awareness</p>
         <a id="scroll_btn" onClick={() => scrollById("about", true)}>
           ▼ learn about climate ▼
